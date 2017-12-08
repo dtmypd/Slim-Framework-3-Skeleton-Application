442c3cb1d4f18b9d1221c8991b6b9e6aa631e6b6
Balazs Krizsan <krizsan.balazs@gmail.com>
2017-12-08 10:05:42 +0100

    Route refact

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,24 @@
 use App\Controllers\TodoListController;
 use App\Controllers\UserController;
 
-$app->group(
-    '/todo-list',
+$app->group('/v1',
     function ()
     {
-        $this->post('/create', TodoListController\CreateAction::class);
-        $this->get('', TodoListController\ListAction::class);
-        $this->get('/{id}', TodoListController\ShowAction::class);
-    }
-);
+        $this->group(
+            '/todo-list',
+            function ()
+            {
+                $this->post('/create', TodoListController\CreateAction::class);
+                $this->get('', TodoListController\ListAction::class);
+                $this->get('/{id}', TodoListController\ShowAction::class);
+            }
+        );
 
-$app->group('/users',
-    function ()
-    {
-        $this->post('/create', UserController\CreateAction::class);
+        $this->group('/users',
+            function ()
+            {
+                $this->post('/create', UserController\CreateAction::class);
+            }
+        );
     }
 );
