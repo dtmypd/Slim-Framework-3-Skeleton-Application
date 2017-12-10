4ddbce34814ec47ba23fe2a2fc2069ba8ab334c6
Balazs Krizsan <krizsan.balazs@gmail.com>
2017-12-10 21:30:13 +0100

    Remove old test

--- tests/EndToEnd/Web/Index/IndexActionTest.php
+++ tests/EndToEnd/Web/Index/IndexActionTest.php
@@ -27,26 +27,4 @@ class IndexActionTest extends AbstractEndToEndTest
         $this->assertEquals($expectedStatusCode, $response->getStatusCode(), 'Status code mismatch.');
         $this->assertEquals($expectedResponseBody, (string)$response->getBody(), 'Response body mismatch.');
     }
-
-    /**
-     * @test
-     *
-     * @throws Exception
-     * @throws MethodNotAllowedException
-     * @throws NotFoundException
-     */
-    public function qwe()
-    {
-        // Arrange
-        $getUri               = '/';
-        $expectedResponseBody = 'HTML part can go later with Twig!';
-        $expectedStatusCode   = 200;
-
-        // Act
-        $response = $this->createGetRequest($getUri);
-
-        // Assert
-        $this->assertEquals($expectedStatusCode, $response->getStatusCode(), 'Status code mismatch.');
-        $this->assertEquals($expectedResponseBody, (string)$response->getBody(), 'Response body mismatch.');
-    }
 }
