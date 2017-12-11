d68bce68cdc8699b54948b5dece1c09ca368f1e6
Balazs Krizsan <krizsan.balazs@gmail.com>
2017-12-12 00:51:17 +0100

    Action clean up#2

--- src/App/Controllers/Api/v1/TodoListController/CreateAction.php
+++ src/App/Controllers/Api/v1/TodoListController/CreateAction.php
@@ -12,32 +12,23 @@ use ExtendedSlim\Http\RestApiResponse;
 
 class CreateAction extends AbstractAction
 {
-    /** @var TodoListService */
-    private $todoListService;
-
-    /** @var ValidatorFactory */
-    private $validatorFactory;
-
     /**
+     * @param Request          $request
+     * @param Response         $response
      * @param TodoListService  $todoListService
      * @param ValidatorFactory $validatorFactory
-     */
-    public function __construct(TodoListService $todoListService, ValidatorFactory $validatorFactory) {
-        $this->todoListService  = $todoListService;
-        $this->validatorFactory = $validatorFactory;
-    }
-
-    /**
-     * @param Request  $request
-     * @param Response $response
      *
      * @return Response
      * @throws Exception
      */
-    public function __invoke(Request $request, Response $response): Response
-    {
+    public function __invoke(
+        Request $request,
+        Response $response,
+        TodoListService $todoListService,
+        ValidatorFactory $validatorFactory
+    ): Response {
         $createRequest = new CreateRequest($request->getParam('name'), $request->getParam('user_id'));
-        $violations    = $this->validatorFactory->create()->validate($createRequest);
+        $violations    = $validatorFactory->create()->validate($createRequest);
 
         if ($violations->count() > 0)
         {
@@ -51,7 +42,7 @@ class CreateAction extends AbstractAction
             );
         }
 
-        $restApiResponse = $this->todoListService->create($request->getParam('name'), $request->getParam('user_id'));
+        $restApiResponse = $todoListService->create($request->getParam('name'), $request->getParam('user_id'));
 
         return $response->createRestApiResponse($restApiResponse);
     }
