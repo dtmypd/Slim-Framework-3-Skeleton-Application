30940ead9d1e794d6f79b31a5666331cd1b407dd
Budai Gábor <gabor.budai@axelspringer.hu>
2018-01-02 18:42:38 +0100

    Cast request params to string

--- src/ExtendedSlim/Http/Request.php
+++ src/ExtendedSlim/Http/Request.php
@@ -2,4 +2,18 @@
 
 class Request extends \Slim\Http\Request
 {
+    /**
+     * Fetch request parameter value from body or query string (in that order) and cast it to string.
+     *
+     * Note: This is an extended method of the slim Request::getParam method.
+     *
+     * @param string $key     The parameter key.
+     * @param string $default The default value.
+     *
+     * @return mixed The parameter value as a string.
+     */
+    public function getParam($key, $default = null)
+    {
+        return (string)parent::getParam($key, $default);
+    }
 }
