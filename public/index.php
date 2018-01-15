0c29bb6339c14fc8e8b3267cf404fa58169a8f45
Balazs Krizsan <krizsan.balazs@gmail.com>
2018-01-15 17:52:58 +0100

    Block CLI calls on public index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
 <?php
-//if (PHP_SAPI == 'cli-server') {
-//    // To help the built-in PHP dev server, check if the request was actually for
-//    // something which should probably be served as a static file
-//    $url  = parse_url($_SERVER['REQUEST_URI']);
-//    $file = __DIR__ . $url['path'];
-//    if (is_file($file)) {
-//        return false;
-//    }
-//}
+
+if (in_array(PHP_SAPI , ['cli', 'cli-server'])) {
+    print "\n\n" . 'Error: CLI calls are not allowed on the public entry point.' . "\n\n";
+
+    exit;
+}
 
 use ExtendedSlim\App;
 use ExtendedSlim\App\Config;
